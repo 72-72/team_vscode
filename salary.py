f6627be09e5e08f4a1fc0dab061d9b560ad54fd3
koyachi syusuke <s.koyachi.sys20@morijyobi.ac.jp>
2021-03-29 11:51:23 +0900

    名前の変更

--- salary.py
+++ salary.py
@@ -4,14 +4,14 @@ app = Flask(__name__)
 
 @app.route("/Salary")
 def Salary():
-    return render_template("index.html")
+    return render_template("salary_index.html")
 
-@app.route("/result")
-def result():
+@app.route("/salary_result")
+def salary_result():
     x = int(request.args.get("x"))
     y = int(request.args.get("y"))
     z = x * y
-    return render_template("result.html",result=z)
+    return render_template("salaty_result.html",result=z)
 
 if __name__ == "__main__":
     app.run(debug=True)
