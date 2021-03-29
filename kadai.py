539d68338dbf8e534795191eb4b74787c6f9ceed
isowa saki <s.isowa.sys20@morijyobi.ac.jp>
2021-03-29 11:35:10 +0900

    ファイル名の変更
    ファイル名を変更しました。

--- kadai.py
+++ kadai.py
@@ -4,14 +4,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def top():
-    return render_template("index.html")
+    return render_template("kadai1.html")
 
 @app.route("/result")
 def result():
     han = int(request.args.get("han"))
     menseki = han * han * 3.14
     ensyu = 2 * han * 3.14
-    return render_template("result.html",menseki = menseki,ensyu = ensyu)
+    return render_template("kadai2.html",menseki = menseki,ensyu = ensyu)
     
 if __name__ == "__main__":
     app.run(debug=True) 
